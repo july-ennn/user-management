a06484567bc44ac8d7db68dd3233e043e34715f4
Julie <julieformalejo@gmail.com>
2025-05-22 11:03:05 +0800

    fix issue of undefined variable $slot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,14 +22,14 @@
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        {{ $header ?? '' }}
                     </div>
                 </header>
             @endif
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                {{ $slot ?? '' }}
             </main>
         </div>
     </body>
