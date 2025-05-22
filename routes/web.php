842dcd2474d7461360ee06330b17f1a304d2fd1d
Julie <julieformalejo@gmail.com>
2025-05-22 13:08:01 +0800

    fix issue of users page not showing

--- routes/web.php
+++ routes/web.php
@@ -4,33 +4,26 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Public route - welcome page
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Dashboard route - requires authentication and email verification
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Group routes that require authentication
 Route::middleware('auth')->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::middleware(['auth'])->group(function(){
+    // User management routes (CRUD)
     Route::resource('users', UserController::class);
 });
 
+// Auth routes (login, register, password reset, etc.)
 require __DIR__.'/auth.php';
